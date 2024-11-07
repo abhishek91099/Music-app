c68c7b522147c68a250f24f345c8e5882116e6a6
abhishek91099 <48791106+abhishek91099@users.noreply.github.com>
2024-11-07 20:28:54 +0530

    Delete backend/playerBackend directory

--- backend/playerBackend/musicplayerBackend/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Songs(models.Model):
-    name=models.CharField(max_length=20)
-    genre=models.CharField()
-
-    def __str__(self) -> str:
-        return self.name
